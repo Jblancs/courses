c929bf543c83ab17a76f8f5a1d50e2c445be9625
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2024-07-17 19:34:03 -0400

    added first test

--- courses/udemy-TESTING-LIBRARY/vite-starter/src/App.test.jsx
+++ courses/udemy-TESTING-LIBRARY/vite-starter/src/App.test.jsx
@@ -1,8 +1,25 @@
+import { logRoles } from "@testing-library/react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("App contains correct heading", () => {
-  render(<App />);
-  const headingElement = screen.getByRole("heading", {name: /learn react/i});
-  expect(headingElement).toBeInTheDocument();
+test("button starts with correct color", () => {
+  const {container} = render(<App/>);
+  // use logRoles to help figure out what roles to use
+  // logRoles(container)
+  
+  // adding name plays role of assertion and throws error if button is not found with text including blue
+  const buttonElement = screen.getByRole("button", {name: /blue/i});
+  expect(buttonElement).toHaveClass("red")
+});
+
+test("button starts with correct text", () => {
+
+});
+
+test("button has correct color after click", () => {
+
+});
+
+test("button has correct text after click", () => {
+
 });
